4740a31072a06d6242ae06a2c95f7a9e95217844
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-04-22 10:36:23 -0500

    First Commit

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
 public class Employee {
+
+        //Fields (store data for each employee)
+
+        private int employeeId;      //stores employee ID
+        private String name;         //stores employee name
+        private double hoursWorked;  // store hours worked
+        private double payRate;      //stores pay rate per hour
+
+        //Constructor
+        //Used to create a new employee object
+        public Employee (int employeeId, String name, double hoursWorked, double payRate){
+            this.employeeId = employeeId;    //assign ID
+            this.name = name;                //assign name
+            this.hoursWorked = hoursWorked;   //assign hours worked
+            this.payRate = payRate;          //assign pay rate
+        }
+    //Gettter methods (used to access private data)
+        public int getEmployeeId() {
+            return employeeId;               //return employee ID
+        }
+        public String getName() {
+            return name;                    //return employee name
+        }
+        public double getHoursWorked() {
+            return hoursWorked;             //return hours worked
+        }
+        public double getPayRate() {
+            return payRate;                 //return pay aret
+        }
+    //Method to calculate gross pay
+        public double getGrossPay(){
+            //Formula: Gross Pay = Hours worked * Pay Rate
+            return hoursWorked * payRate;
+    }
 }
--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,62 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.IOException;
+
 public class PayrollCalculator {
+    public static void main(String[] args) {
+
+        try {
+            //Step 1: Load the CSV file from resources folder
+
+            InputStream is = PayrollCalculator.class.getResourceAsStream("/employees.csv");
+
+            //Check if file exists
+            if (is == null){
+                System.out.println("File not found!");
+                return;                                 //Stop program if file is missing
+            }
+            //Wrap InputStream with BufferedReader for reading text
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+            //Step 2: Skip the header line
+            reader.readLine();
+
+            String line;                             //Variable to store each line from file
+
+            //Step 3: Read file line by line
+            while ((line = reader.readLine()) != null){
+
+                //Step 4: Split the line into parts
+                String[] tokens = line.split("\\|");
+
+                //Step 5: Convert string values to proper types
+                int id = Integer.parseInt(tokens[0]);
+                //Convert "10" to 10
+
+                String name = tokens [1];
+                //name stays as String
+
+                double hoursWorked = Double.parseDouble(tokens[2]);
+                //Convert "52.5" to 52.5
+
+                double payRate = Double.parseDouble(tokens[3]);
+                //Convert "12.50" to 12.50
+
+                //Step 6: Create an employee object
+                Employee emp = new Employee(id, name, hoursWorked, payRate);
+
+                //Step 7: Display employee information
+                System.out.printf("ID: %d  |  Name: %s  | Gross: %.2f\n",
+                        emp.getEmployeeId(),   //get ID
+                        emp.getName(),         //get name
+                        emp.getGrossPay());    //calculate gross pay
+            }
+            reader.close();
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
